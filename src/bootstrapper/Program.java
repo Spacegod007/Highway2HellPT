8fdbe5423dbf0c71f2396b260a90aa58c0b0ebd5
Spacegod007 <spacegod007@outlook.com>
2017-10-18 11:07:37 +0200

    Package bootstrapper: - Added Program class, a main class to initiate the entire application - Note: doesn't contain an implementation at this moment - Removed temp.gitdiscovery file

---
+++ src/bootstrapper/Program.java
@@ -0,0 +1,12 @@
+package bootstrapper;
+
+/**
+ * Bootstrapper class to initiate the entire application
+ */
+public class Program
+{
+    public static void main(String[] args)
+    {
+        //TODO: initiating the entire application from this point
+    }
+}
